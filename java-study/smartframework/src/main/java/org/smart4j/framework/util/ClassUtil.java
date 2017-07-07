646d92029636c5a9818580412a32be2771910af9
x4vierli <x4vierli@gmail.com>
2017-07-08 00:02:15 +0800

    add getClassSet method

--- java-study/smartframework/src/main/java/org/smart4j/framework/util/ClassUtil.java
+++ java-study/smartframework/src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -3,7 +3,15 @@ package org.smart4j.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Author: LiTing
@@ -30,7 +38,40 @@ public final class ClassUtil {
     }
 
     public static Set<Class<?>> getClassSet(String packageName) {
-
+        Set<Class<?>> classSet = new HashSet<>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")) {
+                        String packagePath = url.getPath().replaceAll("%20", " ");
+                        addClass(classSet, packagePath, packageName);
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        if (jarURLConnection != null) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if (jarFile != null) {
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if (jarEntryName.endsWith(".class")) {
+                                        String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replaceAll("/", ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("get class set failure", e);
+            throw new RuntimeException(e);
+        }
+        return classSet;
     }
 
 }
