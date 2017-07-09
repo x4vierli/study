19f20a6212090d95a8975a758682f8c2bd75c21a
x4vierli <x4vierli@gmail.com>
2017-07-09 21:32:22 +0800

    add @Action, @Controller, @Inject, @Service annotations

---
+++ java-study/smartframework/src/main/java/org/smart4j/framework/annotation/Service.java
@@ -0,0 +1,15 @@
+package org.smart4j.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Author: LiTing
+ * Date: 2017/7/9 21:30
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Service {
+}
