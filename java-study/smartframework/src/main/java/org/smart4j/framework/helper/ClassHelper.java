4253b8f91580394161f6db0cd09aaf38cb34c09b
x4vierli <x4vierli@gmail.com>
2017-07-09 22:14:55 +0800

    add classhelper

---
+++ java-study/smartframework/src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -0,0 +1,53 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.smart4j.framework.annotation.Controller;
+import org.smart4j.framework.annotation.Service;
+
+/**
+ * Author: LiTing
+ * Date: 2017/7/9 21:33
+ */
+public final class ClassHelper {
+
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+}
