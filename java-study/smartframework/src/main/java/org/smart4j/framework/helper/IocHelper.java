420c2bb34c713afb744b2a0eecc751f7b7790920
x4vierli <x4vierli@gmail.com>
2017-07-10 23:55:04 +0800

    阅读切面

--- java-study/smartframework/src/main/java/org/smart4j/framework/helper/IocHelper.java
+++ java-study/smartframework/src/main/java/org/smart4j/framework/helper/IocHelper.java
@@ -27,6 +27,7 @@ public final class IocHelper {
                             Object beanFieldInstance = beanMap.get(beanFieldClass);
                             if (beanFieldInstance != null) {
                                 ReflectionUtil.setField(beanInstance, beanField, beanFieldInstance);
+
                             }
                         }
                     }
