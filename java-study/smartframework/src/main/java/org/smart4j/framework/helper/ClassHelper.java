df9183551b19b8819ee4aebbf746559ea49d453b
x4vierli <x4vierli@gmail.com>
2017-07-11 23:46:21 +0800

    add method to BeanHelper and ClassHelper

--- java-study/smartframework/src/main/java/org/smart4j/framework/helper/ClassHelper.java
+++ java-study/smartframework/src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -2,6 +2,7 @@ package org.smart4j.framework.helper;
 
 import org.smart4j.framework.util.ClassUtil;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Set;
 import org.smart4j.framework.annotation.Controller;
@@ -49,5 +50,38 @@ public final class ClassHelper {
         beanClassSet.addAll(getServiceClassSet());
         beanClassSet.addAll(getControllerClassSet());
         return beanClassSet;
+    }
+
+    /**
+     * 获取应用包名下某父类（或接口）的所有子类（或实现类）
+     * @param superClass
+     * @return
+     */
+
+    public static Set<Class<?>> getClassSetBySuper(Class<?> superClass) {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (superClass.isAssignableFrom(cls) && !superClass.equals(cls)) {
+                classSet.add(cls);
+            }
+        }
+
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下带有某注解的所有类
+     * @param annotationClass
+     * @return
+     */
+
+    public static Set<Class<?>> getClassSetByAnnotation(Class<? extends Annotation> annotationClass)a {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(annotationClass)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
     }
 }
