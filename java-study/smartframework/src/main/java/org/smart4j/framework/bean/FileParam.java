48a5975dd22f20168e9ba14a1df5540f529f609a
x4vierli <x4vierli@gmail.com>
2017-07-13 00:08:41 +0800

    add FileParam

---
+++ java-study/smartframework/src/main/java/org/smart4j/framework/bean/FileParam.java
@@ -0,0 +1,43 @@
+package org.smart4j.framework.bean;
+
+import java.io.InputStream;
+
+/**
+ * Author: LiTing
+ * Date: 2017/7/12 23:50
+ */
+public class FileParam {
+    private String fieldName;
+    private String fileName;
+    private long fileSize;
+    private String contentType;
+    private InputStream inputStream;
+
+    public FileParam(String fieldName, String fileName, long fileSize, String contentType, InputStream inputStream) {
+        this.fieldName = fieldName;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.contentType = contentType;
+        this.inputStream = inputStream;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+}
