e8f3d60baa85a4a74ac3941f0eb4a1efa4b573da
x4vierli <x4vierli@gmail.com>
2017-07-13 23:42:36 +0800

    add RequestHelper and update Method

--- java-study/smartframework/src/main/java/org/smart4j/framework/DispatcherServlet.java
+++ java-study/smartframework/src/main/java/org/smart4j/framework/DispatcherServlet.java
@@ -4,9 +4,7 @@ import org.smart4j.framework.bean.Data;
 import org.smart4j.framework.bean.Handler;
 import org.smart4j.framework.bean.Param;
 import org.smart4j.framework.bean.View;
-import org.smart4j.framework.helper.BeanHelper;
-import org.smart4j.framework.helper.ConfigHelper;
-import org.smart4j.framework.helper.ControllerHelper;
+import org.smart4j.framework.helper.*;
 import org.smart4j.framework.util.*;
 
 import javax.servlet.ServletConfig;
@@ -38,6 +36,7 @@ public class DispatcherServlet extends HttpServlet {
         jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
         ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
         defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*");
+        UploadHelper.init(servletContext);
     }
 
     @Override
@@ -45,33 +44,22 @@ public class DispatcherServlet extends HttpServlet {
         String requestMethod = request.getMethod().toLowerCase();
         String requestPath = request.getPathInfo();
 
+        if (requestPath.equals("/favicon.ico")) {
+            return;
+        }
+
         Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
         if (handler != null) {
             Class<?> controllerClass = handler.getControllerClass();
             Object controllerBean = BeanHelper.getBean(controllerClass);
 
-            Map<String, Object> paramMap = new HashMap<>();
-            Enumeration<String> paramNames = request.getParameterNames();
-            while (paramNames.hasMoreElements()) {
-                String paramName = paramNames.nextElement();
-                String paramValue = request.getParameter(paramName);
-                paramMap.put(paramName, paramValue);
+            Param param;
+            if (UploadHelper.isMultipart(request)) {
+                param = UploadHelper.createParam(request);
+            } else {
+                param = RequestHelper.createParam(request);
             }
-            String body = CodecUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
-            if (StringUtil.isNotEmpty(body)) {
-                String[] params = StringUtil.splitString(body, "&");
-                if (ArrayUtil.isNotEmpty(params)) {
-                    for (String param : params) {
-                        String[] array = StringUtil.splitString(param, "=");
-                        if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
-                            String paramName = array[0];
-                            String paramValue = array[1];
-                            paramMap.put(paramName, paramValue);
-                        }
-                    }
-                }
-            }
-            Param param = new Param(paramMap);
+
             Object result;
             Method actionMethod = handler.getActionMethod();
             if (param.isEmpty()) {
@@ -79,33 +67,40 @@ public class DispatcherServlet extends HttpServlet {
             } else {
                 result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
             }
+
             if (result instanceof View) {
-                View view = (View) result;
-                String path = view.getPath();
-                if (StringUtil.isNotEmpty(path)) {
-                    if (path.startsWith("/")) {
-                        response.sendRedirect(request.getContextPath() + path);
-                    } else {
-                        Map<String, Object> model = view.getModel();
-                        for (Map.Entry<String, Object> entry :model.entrySet()) {
-                            request.setAttribute(entry.getKey(), entry.getValue());
-                        }
-                        request.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(request, response);
-                    }
-                }
+                handleViewResult((View) result, request, response);
             } else if (result instanceof Data) {
-                Data data = (Data) result;
-                Object model = data.getModel();
-                if (model != null) {
-                    response.setContentType("application/json");
-                    response.setCharacterEncoding("UTF-8");
-                    PrintWriter writer = response.getWriter();
-                    String json = JsonUtil.toJson(model);
-                    writer.write(json);
-                    writer.flush();
-                    writer.close();
-                }
+                handleDataResult((Data) result, response);
             }
+        }
+    }
+
+    private void handleViewResult(View view, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String path = view.getPath();
+        if (StringUtil.isNotEmpty(path)) {
+            if (path.startsWith("/")) {
+                response.sendRedirect(request.getContextPath() + path);
+            } else {
+                Map<String, Object> model = view.getModel();
+                for (Map.Entry<String, Object> entry : model.entrySet()) {
+                    request.setAttribute(entry.getKey(), entry.getValue());
+                }
+                request.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(request, response);
+            }
+        }
+    }
+
+    private void handleDataResult(Data data, HttpServletResponse response) throws IOException {
+        Object model = data.getModel();
+        if (model != null) {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter writer = response.getWriter();
+            String json = JsonUtil.toJson(model);
+            writer.write(json);
+            writer.flush();
+            writer.close();
         }
     }
 }
