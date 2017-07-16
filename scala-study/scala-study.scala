aeaa28bdf281182c74d9e468f26f8f4a433207f2
x4vierli <x4vierli@gmail.com>
2017-07-16 22:11:02 +0800

    add scala code

--- scala-study/scala-study.scala
+++ scala-study/scala-study.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable.ArrayBuffer
 def abs(x: Double) = if (x >= 0) x else -x
 
 def decorate(str: String, left: String = "[", right: String = "]") = left + str + right
@@ -15,6 +16,27 @@ def box(s: String) {
     println(border + "|" + s + "|\n" + border)
 }
 
-box("Hello")
-
 val nums = new Array[Int](10)
+val s = Array("Hello", "World")
+
+val b = ArrayBuffer[Int]()
+b += 1
+println(b)
+b += (1, 2, 3, 5)
+println(b)
+b ++= Array(8, 13, 21)
+println(b)
+b.trimEnd(5)
+println(b)
+b.insert(2, 6)
+println(b)
+b.insert(2, 7, 8, 9)
+println(b)
+b.remove(2)
+println(b)
+b.remove(2, 3)
+println(b)
+
+val aa = Array(2, 3, 5, 7, 11)
+val aaResult = for (elem <- aa) yield 2 * elem
+println(aaResult[1]) 
