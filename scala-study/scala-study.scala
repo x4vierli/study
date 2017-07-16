ea1821f0805b73c68be4c63a357958d279bb0725
x4vierli <x4vierli@gmail.com>
2017-07-16 22:51:30 +0800

    add scala code

--- scala-study/scala-study.scala
+++ scala-study/scala-study.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.ArrayBuffer
+
 def abs(x: Double) = if (x >= 0) x else -x
 
 def decorate(str: String, left: String = "[", right: String = "]") = left + str + right
@@ -21,22 +22,20 @@ val s = Array("Hello", "World")
 
 val b = ArrayBuffer[Int]()
 b += 1
-println(b)
 b += (1, 2, 3, 5)
-println(b)
 b ++= Array(8, 13, 21)
-println(b)
 b.trimEnd(5)
-println(b)
 b.insert(2, 6)
-println(b)
 b.insert(2, 7, 8, 9)
-println(b)
 b.remove(2)
-println(b)
 b.remove(2, 3)
-println(b)
 
 val aa = Array(2, 3, 5, 7, 11)
 val aaResult = for (elem <- aa) yield 2 * elem
-println(aaResult[1]) 
+val testSum = Array(1, 7, 2, 9).sum
+
+val bufferResult = ArrayBuffer("Mary", "had", "a", "little", "lamb").max
+val bb = ArrayBuffer(1, 7, 2, 9)
+val bbStr = bb.mkString("<", ",", ">")
+
+val matrix = Array.ofDim[Double](3, 4)
