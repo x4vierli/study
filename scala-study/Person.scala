c7355c06115926b4be09cb8b5aac1d2c48d04194
x4vierli <x4vierli@gmail.com>
2017-07-27 00:21:12 +0800

    add scala code

--- scala-study/Person.scala
+++ scala-study/Person.scala
@@ -1,5 +1,23 @@
 import scala.beans.BeanProperty
 
-class Person {
-    @BeanProperty var name: String = _
+class Person(val name: String, val age: Int) {
+    println("Just constructed another person")
+    def description = name + " is " + age + " years old" 
+    // // @BeanProperty var name: String = _
+    // private var name = ""
+    // private var age = 0
+
+    // def this(name: String) {
+    //     this()
+    //     this.name = name
+    // }
+
+    // def this(name: String, age: Int) {
+    //     this(name)
+    //     this.age = age
+    // }
 }
+
+// val p1 = new Person
+// val p2 = new Person("Fred")
+val p3 = new Person("Fred", 42)
