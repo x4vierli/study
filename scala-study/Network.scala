482ce27b616c2e7810a8e92552203ea909d272a3
x4vierli <x4vierli@gmail.com>
2017-07-27 00:37:43 +0800

    add scala code

---
+++ scala-study/Network.scala
@@ -0,0 +1,24 @@
+import scala.collection.mutable.ArrayBuffer
+
+class Network {
+    class Member(val name: String) {
+        val contacts = new ArrayBuffer[Member]
+    } 
+
+    private val members = new ArrayBuffer[Member]
+
+    def join(name: String) = {
+        val m = new Member(name)
+        members += m
+        m
+    }
+}
+
+val chatter = new Network
+val myFace = new Network
+
+val fred = chatter.join("Fred")
+val wilma = chatter.join("Wilma")
+fred.contacts += wilma
+val barney = myFace.join("Barney")
+fred.contacts += barney
